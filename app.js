1d59a52be268941a5147060ae970e88ab888167c
John Sung <johnsungjs@gmail.com>
2023-12-18 11:00:17 +0700

    add dotenv

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 // disini udah dapet path, headers, dan body, tinggal axios aje udah
 app.post('/*', (req, res) => {
